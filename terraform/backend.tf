08eb7ad11d4949f69d4ecf6a46efaba1e2dff031
Thakre <nikita.thakre@capgemini.com>
2026-06-01 08:19:06 +0530

    Update region to us-east-1 in S3 backend configuration

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "eks-terraform-state-bucket"
     key            = "eks/terraform.tfstate"
-    region         = "ap-south-1"
+    region         = "us-east-1"
     dynamodb_table = "terraform-lock-table"
     encrypt        = true
   }
